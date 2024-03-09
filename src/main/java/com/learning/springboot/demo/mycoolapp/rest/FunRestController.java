b7da45fc806aa7230c523318d4377a6bc0b929ff
thangnd <thangit96@gmail.com>
2024-03-09 14:32:54 +0700

    05-changing-spring-config

--- src/main/java/com/learning/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/learning/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.learning.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties value coach.name team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
